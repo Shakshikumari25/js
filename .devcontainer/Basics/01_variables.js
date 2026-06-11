688bbfa92872abcc7fdb795558507c3298cea7c3
Shakshikumari25 <kumarishakshi1625@gmail.com>
2026-06-11 15:31:01 +0000

    variables

---
+++ .devcontainer/Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 133356
+let accountEmail = "hdsfdsfjk@gamil.com" //variables by var and let(mostly)
+var accountpassword = "12233" //issue in block and funct. scope
+accountCity = "Jaipur" //by default variable(not preffered)
+let accountState;
+
+// accountId = 23
+accountEmail = "dhfd.com"
+accountpassword = "5866784"
+accountCity = "Delhi"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountpassword,accountCity,accountState])
