3713cb4db97ca5185c0e8052ca63b2fd0643b984
Shakshikumari25 <kumarishakshi1625@gmail.com>
2026-06-11 15:59:00 +0000

    dataTypes

---
+++ .devcontainer/Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS Code as newer version
+
+// alert(3+3) we are using nodejs, not browser
+
+//keep the code readable
+console.log(7+8);
+console.log("SHAKSHI");
+
+let name = "Shakshi"
+let age = 21
+let isActive = false
+
+// number
+// bigint
+// string
+// boolean
+// null => standalone value
+// undefined => not defined
+// symbol => unique
+
+// object
+
+console.log(typeof "shs")
+console.log(typeof age)
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
