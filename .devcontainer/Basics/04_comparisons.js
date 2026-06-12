028edac324fd98edb1235db621ad33c42e197bb3
Shakshikumari25 <kumarishakshi1625@gmail.com>
2026-06-12 20:03:41 +0000

    comparisons

---
+++ .devcontainer/Basics/04_comparisons.js
@@ -0,0 +1,17 @@
+// console.log(2>1);
+// console.log(2==1);
+
+// console.log("2">1);
+// console.log("02" > 1); //we can not compare two different data types
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined >= 0);
+
+console.log("2" === 2); //use === to avoid data type conversions automatically
+
+//don't use normal comparison 
