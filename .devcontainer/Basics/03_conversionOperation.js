579d601dcf7daa2f8c0ba1957bb6a3c5bca04a15
Shakshikumari25 <kumarishakshi1625@gmail.com>
2026-06-12 19:55:32 +0000

    operations

--- .devcontainer/Basics/03_conversionOperation.js
+++ .devcontainer/Basics/03_conversionOperation.js
@@ -1,30 +1,61 @@
-let score = "45"
+// let score = "45"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-let marks = "67ad"
-console.log(typeof marks);
+// let marks = "67ad"
+// console.log(typeof marks);
 
-let Val = Number(marks)
-console.log(typeof Val)
-console.log(Val); //we will get NaN(not a number) as val doesnot contains number only
-
-
-let isLoggedIn = 1
-
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let Val = Number(marks)
+// console.log(typeof Val)
+// console.log(Val); //we will get NaN(not a number) as val doesnot contains number only
 
 
-// 1 => true and 0 =>false
-// "" =>false and "someValue" =>true
+// let isLoggedIn = 1
 
-let age = 78
-let stringNum = String(age)
-console.log(typeof stringNum)
-console.log(stringNum)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// // 1 => true and 0 =>false
+// // "" =>false and "someValue" =>true
+
+// let age = 78
+// let stringNum = String(age)
+// console.log(typeof stringNum)
+// console.log(stringNum)
+
+//operations
+
+// let value = 3
+// let negVal = -value
+// console.log(negVal);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+
+// let str1 = "Hello"
+// let str2 = " Shakshi"
+// console.log(str1+str2);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+3);
+// console.log(1+4+"2");
+
+// console.log(true);
+// console.log(+true);
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
