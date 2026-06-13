586f17d395f934fc3aa8c5d4aeaca07edfba2def
Shakshikumari25 <kumarishakshi1625@gmail.com>
2026-06-13 17:44:00 +0000

    array

---
+++ .devcontainer/Basics/07_array.js
@@ -0,0 +1,17 @@
+const myArray = [3,5,7,89,1]
+
+console.log(myArray[2]);
+
+const myArr2 = new Array(2,5,6,7,9)
+console.log(myArr2);
+console.log(myArr2.length);
+
+// myArray.push(7);
+// console.log(myArray);
+// myArray.pop()
+// console.log(myArray);
+
+// myArray.unshift(66)
+
+console.log(myArray.includes(9));
+console.log(myArray);
