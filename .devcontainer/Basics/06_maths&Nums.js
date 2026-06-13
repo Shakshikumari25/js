6bd102e54624409e1099fd0412e32a3fe4070300
Shakshikumari25 <kumarishakshi1625@gmail.com>
2026-06-13 15:58:26 +0000

    date and time

--- .devcontainer/Basics/06_maths&Nums.js
+++ .devcontainer/Basics/06_maths&Nums.js
@@ -25,11 +25,41 @@ const hundreds = 10000000
 // console.log(Math.max(4,3,5,8,9));
 
 
-console.log(Math.random()); //value between 0 and 1
-console.log((Math.random()*10)+1); //btn 1 and 10
-console.log(Math.floor(Math.random()*10)+1);
+// console.log(Math.random()); //value between 0 and 1
+// console.log((Math.random()*10)+1); //btn 1 and 10
+// console.log(Math.floor(Math.random()*10)+1);
 
-const min = 10
-const max = 20
+// const min = 10
+// const max = 20
 
-console.log(Math.floor(Math.random() * (max-min+1)) + min);//more customised
+// console.log(Math.floor(Math.random() * (max-min+1)) + min);//more customised
+
+
+//DATES
+
+let myDate = new Date()
+// console.log(myDate.toDateString());
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+let newDate = new Date(2025,0,23)
+// console.log(newDate);
+// console.log(newDate.toLocaleDateString());
+
+let myCreatedDate = new Date("01-14-2026")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000));
+
+console.log(myCreatedDate.getMonth());
+console.log(myCreatedDate.getDay());
+
+newDate.toLocaleString('default',{
+    weekday: "long"
+})
