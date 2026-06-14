24cb6dcdb29c91ab2c059af5349c20bbc913c20b
Shakshikumari25 <kumarishakshi1625@gmail.com>
2026-06-14 15:17:31 +0000

    object2

---
+++ .devcontainer/Basics/09_object2.js
@@ -0,0 +1,51 @@
+const metaUser = new Object()
+
+metaUser.id = "123abc"
+metaUser.name = "Harshit"
+metaUser.isLoggedIn = false
+
+// console.log(metaUser);
+
+const regularUser = {
+    email : "sos@gmail.com",
+    fullname : {
+        userfullname : {
+            firstname : "hitesh",
+            lastname : "choudhary"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.lastname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "v", 4: "c"}
+const obj4 = {5: "d", 6: "h"}
+
+// const obj3 = Object.assign({},obj1,obj2,obj4)
+
+const obj3 = {...obj1,...obj2,...obj4} //spread method is best 
+console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "shakshi@gmail.com"
+    },
+    {
+        id: 2,
+        email: "shahi@gmail.com"
+    },
+    {
+        id: 3,
+        email: "sh@gmail.com"
+    }
+]
+
+console.log(users[0].email);
+
+console.log(Object.keys(metaUser));
+console.log(Object.values(metaUser));
+console.log(Object.entries(metaUser));
+
+console.log(metaUser.hasOwnProperty('isLoggedIn'));
