ac29f0f28ddd4390406ae75d5c63fde03e1a1858
Shakshikumari25 <kumarishakshi1625@gmail.com>
2026-06-15 12:25:12 +0000

    scope

--- .devcontainer/Basics/10_function.js
+++ .devcontainer/Basics/10_function.js
@@ -25,9 +25,9 @@ function loginUserMsg(username){
     return `${username} just logged in`
 }
 
-console.log(loginUserMsg("Shakshi"));
-console.log(loginUserMsg(""));
-console.log(loginUserMsg());
+// console.log(loginUserMsg("Shakshi"));
+// console.log(loginUserMsg(""));
+// console.log(loginUserMsg());
 
 function loginUserMsg(username){
     if(username === undefined){
@@ -37,6 +37,38 @@ function loginUserMsg(username){
     return `${username} just logged in`
 }
 
-console.log(loginUserMsg("Shakshi"));
-console.log(loginUserMsg(""));
-console.log(loginUserMsg());
+// console.log(loginUserMsg("Shakshi"));
+// console.log(loginUserMsg(""));
+// console.log(loginUserMsg());
+
+
+function calculateCartPrice(...num1){ //rest operator
+    return num1
+}
+
+console.log(calculateCartPrice(2, 200, 400, 700));
+
+const user = {
+    userName: "shakshi",
+    price: 300
+}
+
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.userName} and the price is ${anyObject.price}`)
+}
+
+// handleObject(user);
+
+handleObject({
+    userName: "Kriti",
+    price: 599
+})
+
+const myNewArray = [200,400,100,600]
+
+function getSecondVal(getArray){
+    return getArray[1]
+}
+
+// console.log(getSecondVal(myNewArray));
+console.log(getSecondVal([200,400,100,600]));
---
+++ .devcontainer/Basics/11_scopes.js
@@ -0,0 +1,18 @@
+// let a = 10
+// const b = 20
+// var c = 40
+
+if(true){  //block scope
+    let a = 10
+    const b = 20
+    // var c = 30 
+    // c = 90
+    // var and c are not used in block scope as they work out of block also
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+
+
