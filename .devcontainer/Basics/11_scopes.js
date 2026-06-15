8a64c7bb945c371da4e9cd484aea6b37e72b6308
Shakshikumari25 <kumarishakshi1625@gmail.com>
2026-06-15 14:44:08 +0000

    mini hoisting

--- .devcontainer/Basics/11_scopes.js
+++ .devcontainer/Basics/11_scopes.js
@@ -12,7 +12,49 @@ if(true){  //block scope
 
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
 
 
+//CLOSURE
+function one(){
+    const userName = "Shakshi"
 
+    function two(){
+        const website = "youtube"
+        console.log(userName)
+    }
+    // console.log(website)
+
+    //child function can access parent function variable but vice versa not true
+
+    // two()
+}
+
+// one()
+
+if(true){
+    const username  = "kriti"
+    if(true){
+        const website = " facebook"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+console.log(addOne(7)); //we can access function anywhere 
+
+function addOne(num){
+    return num+1
+}
+
+// addOne(8)
+
+// console.log(addTwo(5))  //we can not access expression evrywhere
+
+const addTwo = function(num){ //function cum expression
+    return num+2
+}
+
+console.log(addTwo(4));
