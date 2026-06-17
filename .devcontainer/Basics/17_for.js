0f79e851198331fd3e664d9bde5487e568878639
Shakshikumari25 <kumarishakshi1625@gmail.com>
2026-06-17 09:59:19 +0000

    for loop

---
+++ .devcontainer/Basics/17_for.js
@@ -0,0 +1,44 @@
+// for
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+
+// for(let idx=1; idx<=10; idx++){
+//     console.log(idx);
+//     if(idx == 5){
+//         console.log("DSa");
+//     }
+// }
+
+// for(let idx=1; idx<=10; idx++){
+//     console.log(idx);
+//     if(idx == 5){
+//         console.log("DSa");
+//         break;
+//     }
+// }
+
+// for(let idx=1; idx<=10; idx++){
+//     console.log(idx);
+//     if(idx == 5){
+//         console.log("DSa");
+//         continue;
+//     }
+// }
+
+// for(let i=0; i<=10; i++){
+//     for(let j=0; j<9; j++){
+//         console.log(`${j}, ${i}`);
+//     }
+// }
+
+let myArray = ["aas","sas","dff","regeg","dsda"]
+
+for(let i=0; i<myArray.length; i++){
+    const el = myArray[i];
+    console.log(el);
+}
+
