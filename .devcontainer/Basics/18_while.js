c058eab9e4b3b278943adc255392f58c4cdffd11
Shakshikumari25 <kumarishakshi1625@gmail.com>
2026-06-17 14:57:06 +0000

    while and dowhile

---
+++ .devcontainer/Basics/18_while.js
@@ -0,0 +1,26 @@
+// while loop
+
+// let index = 0
+// while(index <= 10){
+//     console.log(`Value of index is ${index}`);
+//     index = index+2
+// }
+
+// let myArray = ['aaa','sdda','dsdsd','dsffds','dsds']
+
+// let arr = 0
+// while(arr < myArray.length){
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr++
+// }
+
+
+// do while loop
+
+
+let idx = 45
+
+do {
+    console.log(idx);
+    idx++
+} while (idx <= 43);
